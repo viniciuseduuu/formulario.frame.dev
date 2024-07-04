a8dc3e41a41e0fa67ddd162d6300ef0e66bee20e
viniciuseduuu <viniciuseduardohilario@gmail.com>
2024-07-04 13:43:20 -0300

    a

--- src/App.vue
+++ src/App.vue
@@ -45,3 +45,55 @@ header {
   }
 }
 </style>
+<style scoped>
+.altera-titulo 
+<div class="container">
+  <div class="formulario">
+    <h2>Formulário para cadastro do produto</h2>
+    <div class="row">
+      <label for="">Nome:</label>
+      <input type="text" v-model="produto.nome" />
+    </div>
+    <div class="row">
+      <label for="">Preço (em reais):</label>
+      <input type="number" step="0.01" v-model="produto.preco" />
+    </div>
+    <div class="row">
+      <label for="">Quantidade:</label>
+      <input type="number" v-model="produto.quantidade" />
+    </div>
+
+    <fieldset>
+      <legend>Unidade de medida</legend>
+      <div class="items-radiobox">
+        <input type="radio" value="unidade" v-model="produto.medida" /> Unidades
+        <input type="radio" value="peso" v-model="produto.medida" /> Peso
+      </div>
+    </fieldset>
+    <fieldset>
+      <legend>Categorias</legend>
+      <div class="items-checkbox">
+        <template v-for="categoria in categorias" :key="categoria.id">
+          <input
+            type="checkbox"
+            :value="categoria.id"
+            v-model="produto.categorias"
+          />
+           {{ categoria.nome }} 
+        </template>
+      </div>
+    </fieldset>
+    <button @click="mostrarResultado = !mostrarResultado">Mostrar</button>
+  </div>
+</div>
+{
+  background-color: #98e0aa;
+  margin: 1rem 2rem;
+  border-radius: 20px;
+  padding: .75rem;
+  display: flex;
+  gap: 1rem
+}
+</style>
+
+
